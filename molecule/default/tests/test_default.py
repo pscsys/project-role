af02ae6d3b06b22864beec84796e6ae482432edd
Your Name <you@example.com>
2021-07-23 14:22:17 +0200

    added tests and services

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -17,6 +17,17 @@ def test_pkg(host, pkg):
     assert package.is_installed
 
 
+@pytest.mark.parametrize('svc', [
+  'httpd',
+  'firewalld'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
 @pytest.mark.parametrize('file, content', [
 #  ("/etc/firewalld/zones/public.xml", "<service name=\"http\"/>"),
   ("/var/www/html/index.html", "Managed by Ansible")
