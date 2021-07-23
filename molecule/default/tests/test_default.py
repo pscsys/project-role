8241270b2dab15053a3e6728486f7072013bace3
Your Name <you@example.com>
2021-07-23 13:33:38 +0200

    Added infratests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('pkg', [
+  'httpd',
+  'firewalld'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('file, content', [
+#  ("/etc/firewalld/zones/public.xml", "<service name=\"http\"/>"),
+  ("/var/www/html/index.html", "Managed by Ansible")
+])
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
